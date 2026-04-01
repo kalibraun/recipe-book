4f84587afa971a20f130eddc967d44fe18182904
Kali <56895383+kalibraun@users.noreply.github.com>
2026-03-31 19:16:47 -0500

    Use DATABASE_URL env var for PostgreSQL support

--- database.py
+++ database.py
@@ -1,22 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./recipes.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./recipes.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+        engine = create_engine(
+                    SQLALCHEMY_DATABASE_URL,
+                    connect_args={"check_same_thread": False},
+        )
+else:
+        engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase):
-    pass
+        pass
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
+        db = SessionLocal()
+        try:
+                    yield db
+finally:
         db.close()
